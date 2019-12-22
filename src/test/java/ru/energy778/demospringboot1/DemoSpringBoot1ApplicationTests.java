009197cdf3639d903bb9035f8ff39075657b1825
energy778 <one.million.dollars@mail.ru>
2019-12-22 23:40:25 +0300

    init

---
+++ src/test/java/ru/energy778/demospringboot1/DemoSpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.energy778.demospringboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBoot1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
