1da7e1eb7b1fb7d1228fa1ba2bf186563c739abf
energy778 <one.million.dollars@mail.ru>
2019-12-23 16:21:12 +0300

    check ApplicationSettings

--- src/main/java/ru/energy778/demospringboot1/DemoSpringBoot1Application.java
+++ src/main/java/ru/energy778/demospringboot1/DemoSpringBoot1Application.java
@@ -2,12 +2,17 @@ package ru.energy778.demospringboot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoSpringBoot1Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringBoot1Application.class, args);
+        
+        ConfigurableApplicationContext ctx = SpringApplication.run(DemoSpringBoot1Application.class, args);
+        AboutInfoLoader bean = ctx.getBean(AboutInfoLoader.class);
+        System.out.println(bean.getVersion());
+
     }
 
 }
